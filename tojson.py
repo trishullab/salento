a550a3a4bb158393bfe8b5058049aa0d970edebe
Vijayaraghavan Murali <vm4422@gmail.com>
2016-06-19 12:47:46 -0500

    tojson.py: Writing package by package to JSON

--- tojson.py
+++ tojson.py
@@ -10,23 +10,40 @@ def main():
     argparser.add_argument('--output_file', type=str, default=None, required=True,
                        help='output JSON file')
     args = argparser.parse_args()
+    to_json(args)
 
+def to_json(args):
     jsequences = []
-    with open(args.input_file) as fin:
-        for sequence in fin.readlines():
-            jsequences += to_json(sequence)
-    data = { 'data' : jsequences }
-    packages = { 'packages' : [data] }
-    with open(args.output_file, 'w') as fout:
-        fout.write(json.dumps(packages, indent=2))
-    print('Converted {0} sequences to JSON'.format(len(jsequences)))
+    packages = []
+    name = ''
+    nseqs = 0
 
-def to_json(sequence):
+    def add_sequences_to_package():
+        package = { 'data' : jsequences, 'name' : name }
+        if len(package['data']) > 0:
+            packages.append(package)
+        return len(jsequences)
+
+    with open(args.input_file) as fin:
+        for line in fin.readlines():
+            if line[0] == '#':
+                nseqs += add_sequences_to_package()
+                jsequences = []
+                name = line[4:-5]
+            else:
+                jsequences += to_json_sequence(line)
+    nseqs += add_sequences_to_package()
+
+    with open(args.output_file, 'w') as fout:
+        fout.write(json.dumps( { 'packages' : packages }, indent=2))
+    print('Converted {0} packages, {1} sequences to JSON'.format(len(packages), nseqs))
+
+def to_json_sequence(sequence):
     count = int(sequence.split()[0])
     jsequence = []
     for event in ' '.join(sequence.split()[1:]).split(';')[0:-1]:
-        s = re.split('\[|\]', event)
-        call = s[0].replace('"', '')
+        call = event.split('"')[1]
+        s = re.split('\[|\]', event.split('"')[2])
         states = ast.literal_eval('[' + s[1] + ']')
         location = s[3]
         jevent = {'call' : call, 'states' : states, 'location' : location}
