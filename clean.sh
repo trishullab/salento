5a44c6a5f9eba80d3caace2da545bb7cb0052021
Vijayaraghavan Murali <vm4422@gmail.com>
2016-07-12 15:29:05 -0500

    Minor (clean script)

--- clean.sh
+++ clean.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-to_delete="data.npy vocab.pkl config.pkl chars_vocab.pkl primes.pkl weights.h5"
-for file in ${to_delete}
-do
-    find . -iname ${file} | xargs rm -f
-done
+if [ $# -eq 1 -a -d "$1" ]; then
+    dir=$1
+    to_delete="data.npy vocab.pkl config.pkl chars_vocab.pkl primes.pkl weights.h5"
+    for file in ${to_delete}
+    do
+        rm ${dir}/${file}
+    done
+fi
 rm -f *.pyc
 rm -f plots/*
 rm -f save/*
