e8b07224d00d47f291e310b69ccdcd5cead4aa3c
Vijayaraghavan Murali <vm4422@gmail.com>
2016-10-12 16:09:10 -0500

    tojson: Added option --vocab_file
    - tojson will always print call frequencies after every run.
    - Read a set of vocab words from a file. Only vocab words in the data
      will be considered in the output JSON. Other words (stop words) will
      be ignored. Vocab file format compatible with call frequencies output.
    - Print arguments of tojson into the JSON output itself.

--- tojson.py
+++ tojson.py
@@ -4,18 +4,28 @@ import re
 import ast
 import random
 
+counts = {} # count of each call
+vocab = None # vocabulary
+stop_words = [] # complement of vocab
+
 def main():
+    global vocab
     argparser = argparse.ArgumentParser()
     argparser.add_argument('--input_file', type=str, default=None, required=True,
                        help='input file in old format')
     argparser.add_argument('--output_file', type=str, default=None, required=True,
                        help='output JSON file')
+    argparser.add_argument('--vocab_file', type=str, default=None,
+                       help='file containing vocabulary (one per line)')
     argparser.add_argument('--prefixes', action='store_true',
                        help='output all prefixes of each sequence')
     argparser.add_argument('--slice_locations', action='store_true',
                        help="""for each package, generate k packages, where k is the number of locations,
                              that each contain only sequences ending at their respective location""")
     args = argparser.parse_args()
+    if args.vocab_file is not None:
+        with open(args.vocab_file) as f:
+            args.vocab = vocab = list(set([line.split('#')[0] for line in f.readlines()]))
     to_json(args)
 
 def to_json(args):
@@ -50,22 +60,37 @@ def to_json(args):
                 jsequences += to_json_sequence(line, args.prefixes)
     nseqs += add_sequences_to_package()
 
+    if args.vocab_file is not None:
+        args.stop_words = list(stop_words)
+
     with open(args.output_file, 'w') as fout:
-        json.dump( { 'packages' : packages }, fp=fout, indent=2)
+        json.dump( { 'tojson_args' : vars(args), 'packages' : packages }, fp=fout, indent=2)
+
+    print('Call counts (compatible with --vocab_file format):')
+    for w, c in sorted(counts.items(), key=lambda x: -x[1]):
+        print('{}# {}'.format(w, c))
+
     print('\nConverted {0} packages, {1} sequences to JSON'.format(len(packages), nseqs))
 
 def to_json_sequence(sequence, prefixes=False):
+    global stop_words
     jsequence = []
     for event in sequence.split(';')[0:-1]:
         if len(event.split('"')) == 1:
             jevent = {'branches' : int(event)}
         else:
             call = event.split('"')[1]
+            if vocab is not None and call not in vocab:
+                stop_words += [call] if call not in stop_words else []
+                continue
             s = re.split('\[|\]', event.split('"')[2])
             states = ast.literal_eval('[' + s[1] + ']')
             location = s[3]
             jevent = {'call' : call, 'states' : states, 'location' : location}
+            counts[call] = counts[call] + 1 if call in counts else 1
         jsequence.append(jevent)
+    if jsequence == []:
+        return []
     jsequence.append({'call' : 'TERMINAL', 'states' : states, 'location' : 'TERMINAL'})
     if prefixes:
         return [{'sequence' : jsequence[:i+1]} for i in range(len(jsequence))]
